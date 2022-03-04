4359332ee234dd060b6a0e40a51d82ad8ba56fbf
dynamic98 <dynamic98@gist.ac.kr>
2022-03-05 04:58:01 +0900

    socket communication

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const { Server } = require('socket.io')
 const io = new Server(server);
 
 io.on('connection', (socket) => {
-    console.log(socket.id)
+    console.log(socket.id);
+    socket.on('mouse', (data) => {
+        // io.emit('mouse', data)
+        socket.broadcast.emit('mouse', data)
+        // console.log(data);
+    });
 })
 
 server.listen(3000, () => {
