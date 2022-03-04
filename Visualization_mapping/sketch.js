1ae587e445efdf4588216e26f99df3555b236612
dynamic98 <dynamic98@gist.ac.kr>
2022-03-05 04:58:01 +0900

    socket communication

--- Visualization_mapping/sketch.js
+++ Visualization_mapping/sketch.js
@@ -1,18 +1,35 @@
 // const p5 = require('p5');
+// var socket = io;
 
 function setup(){
     createCanvas(400, 400);
     background(51);
-
-    socket = socket.io.connect('https://localhost:3000')
+ 
+    fill(100,100,0);
+    noStroke();
+    ellipse(100, 100, 50, 50);
+    // socket = socket.io.connect('https://localhost:3000')
+    socket.on('mouse', newDrawing);
 }
 
-function draw() {
-    if (mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(255);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
-    
+function newDrawing(data){
+  noStroke();
+  fill(255, 0, 100);
+  ellipse(data.x, data.y, 36, 36);
+}
+
+function mouseDragged(){
+  console.log('Sending: '+ mouseX + ',' + mouseY);
+  var data = {
+    x: mouseX,
+    y: mouseY
+  };
+  socket.emit('mouse', data);
+
+  noStroke(); 
+  fill(100);
+  ellipse(mouseX, mouseY, 36, 36);
+}
+
+function draw() {   
   }
