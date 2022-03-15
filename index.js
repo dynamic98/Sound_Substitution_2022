49f259e294a6500e18098de06ae0437933586ab5
TaeHwa Park <dolavvv@naver.com>
2022-03-15 19:47:00 +0900

    add n test haptic interface

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-app.use(express.static(__dirname+'/Visualization_mapping/sketch2'));
+app.use(express.static(__dirname+'/Visualization_mapping/haptic'));
 console.log(__dirname);
 console.log("My socket sever is running");
 
