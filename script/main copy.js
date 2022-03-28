d039750ac86836f9a9bf051ed663cbe448e31d32
dynamic98 <dynamic98@gist.ac.kr>
2022-03-28 23:04:40 +0900

    정리중..끝이 언제일까..

--- script/main copy.js
+++ script/main copy.js
@@ -10,39 +10,29 @@ let container, stats;
 var CANVAS_WIDTH = 300;
 var CANVAS_HEIGHT = 300;
 
-const shape_heart = document.getElementById("shape_heart");
-const shape_ring = document.getElementById("shape_ring");
-
-var now_geometry;
-var group, geometry, material, compoCenter;
-var ambientLight, spotLight;
-
-var context, src, audio, file, fileLabel;
-var analyser;
-
-
 // BASIC EVENTS
 init();
 animate();
 // REQUIRED FUNCTIONS
 // init function
 function init() {
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
-    renderer = new THREE.WebGLRenderer({ alpha: false, antialias: true });
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
     container = document.getElementById( "container" );
-    container.appendChild( renderer.domElement )
-
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    // renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(300, 300);
+    console.log(CANVAS_WIDTH);
     renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    container.appendChild(renderer.domElement);
+
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(30, CANVAS_WIDTH / CANVAS_HEIGHT, 1, 20000);
+    // camera = new THREE.PerspectiveCamera(45, CANVAS_WIDTH / CANVAS_HEIGHT, 0.1, 100);
+    camera.position.set(1, 20, 100);
 
 
     // Orbit Controller
     controls = new OrbitControls(camera, renderer.domElement);
-    camera.position.set(1, 20, 100);
-
     controls.maxPloarAngle = Math.PI * 0.495;
     controls.target.set( 0, 10, 0 );
     controls.minDistance = 40.0;
@@ -52,18 +42,6 @@ function init() {
     stats = new Stats();
     container.appendChild( stats.dom );
 
-
-    ambientLight = new THREE.AmbientLight(0xaaaaaa);
-    scene.add(ambientLight);
-
-    spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.intensity = 0.9;
-    spotLight.position.set(-10, 40, 20);
-
-    spotLight.castShadow = true;
-    scene.add(spotLight);
-
-    now_geometry = "shape_ring";
 };
 
 
@@ -79,66 +57,15 @@ function animate() {
 // render function
 function render() {
   renderer.render(scene, camera);
-  // console.log('ehy');
 }
 
 
-function createShapeHeart(){
-  var x = 0;
-  var y = 0;
-  var heartShape = new THREE.Shape();
-
-  heartShape.moveTo( x + 5, y + 5 );
-  heartShape.bezierCurveTo( x + 5, y + 5, x + 4, y, x, y );
-  heartShape.bezierCurveTo( x - 6, y, x - 6, y + 7,x - 6, y + 7 );
-  heartShape.bezierCurveTo( x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19 );
-  heartShape.bezierCurveTo( x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7 );
-  heartShape.bezierCurveTo( x + 16, y + 7, x + 16, y, x + 10, y );
-  heartShape.bezierCurveTo( x + 7, y, x + 5, y + 5, x + 5, y + 5 );
-
-  geometry = new THREE.ShapeGeometry(heartShape);
-  material = new THREE.MeshBasicMaterial({
-      color: '#FFFFFF',
-      wireframe: false
-  });
-  compoCenter = new THREE.Mesh(geometry, material);
-  compoCenter.position.set(0, 0, 0);
-  spotLight.lookAt(compoCenter);
-
-  group = new THREE.Group();
-  group.add( compoCenter );
-  scene.add( group );
-}
-
-
-function createShapeRing(){
-  geometry = new THREE.RingGeometry(13, 10, 8, 13, 6, 6.283185307179586);
-  material = new THREE.MeshLambertMaterial({
-    color: '#FFFFFF',
-    wireframe: true,
-    side: THREE.DoubleSide
-  });
-  compoCenter = new THREE.Mesh(geometry, material);
-  compoCenter.position.set(0, 0, 0);
-  spotLight.lookAt(compoCenter);
-
-  group = new THREE.Group();
-  group.add( compoCenter );
-  scene.add( group );
-}
-
-function deleteBasics(){
-  group.remove(compoCenter);
-  compoCenter.geometry.dispose();
-  compoCenter.material.dispose();
-}
 
 // LOAD MUSIC (vizIntit)
 var vizInit = function () {
-  file = document.getElementById("thefile");
-  audio = document.getElementById("audio");
-  fileLabel = document.querySelector("label.file");
-  
+  var file = document.getElementById("thefile");
+  var audio = document.getElementById("audio");
+  var fileLabel = document.querySelector("label.file");
   document.onload = function(e){
     audio.play();
     play();
@@ -148,33 +75,33 @@ var vizInit = function () {
   file.onchange = function(){
     fileLabel.classList.add('normal');
     var files = this.files;
-    console.log("바꼈다 ㅋㅋ");
+
     audio.src = URL.createObjectURL(files[0]);
     audio.load();
     audio.play(); // 음악이 load 되자마자 시각화 요소를 불러옴
     play();  // play - 시각화 요소 불러오기
   }
 
-  context = new AudioContext();
-  src = context.createMediaElementSource(audio);
 
-  var CountingStars = 0;
 
   function play() {
-    analyser = context.createAnalyser();
+    var context = new AudioContext();
+    var src = context.createMediaElementSource(audio);
+    var analyser = context.createAnalyser();
     src.connect(analyser);
     analyser.connect(context.destination);
     analyser.fftSize = 512;
     var bufferLength = analyser.frequencyBinCount;
     var dataArray = new Uint8Array(bufferLength);
-  
+
+
     // meyda analyser
     var chroma = 0;
     var maxChroma = 0;
     var energy = 0;
     var amplitudeSpectrum = 0;
     // var powerSpectrum = 0;
-  
+
     const meyda_analyser = Meyda.createMeydaAnalyzer({
       audioContext: context,
       source: src,
@@ -186,77 +113,119 @@ var vizInit = function () {
         amplitudeSpectrum = features['amplitudeSpectrum']
       }
     })
-    meyda_analyser.start();
-  }
-    
 
- 
-    // document.getElementById('container').appendChild(renderer.domElement);
+    meyda_analyser.start();
+
+    var group = new THREE.Group();
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(0,0,100);
+    camera.lookAt(scene.position);
+    scene.add(camera);
+  
+
+    // var renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+    // renderer.setSize(window.innerWidth, window.innerHeight);
+
+    // Geometry
+    var Geometry = new THREE.RingGeometry(13, 10, 8, 13, 6, 6.283185307179586);
+    var Material = new THREE.MeshLambertMaterial({
+      color: '#FFFFFF',
+      wireframe: true,
+      side: THREE.DoubleSide
+    });
+
+    // * 중요 * Geometry + Material = Mesh(최종) 형태로 바꾸어줌
+    var compoCenter = new THREE.Mesh(Geometry, Material);
+    // var compoLeft = new THREE.Mesh(Geometry, Material);
+    // var compoRight = new THREE.Mesh(Geometry, Material);
+
+    compoCenter.position.set(0, 0, 0);
+    // compoLeft.position.set(-80, 0, 30);
+    // compoRight.position.set(80, 0, 30);
+
+    group.add(compoCenter);
+    // group.add(compoLeft);
+    // group.add(compoRight);
+
+    var ambientLight = new THREE.AmbientLight(0xaaaaaa);
+    scene.add(ambientLight);
+
+    var spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.intensity = 0.9;
+    spotLight.position.set(-10, 40, 20);
+    
+    spotLight.lookAt(compoCenter);
+    // spotLight.lookAt(compoLeft);
+    // spotLight.lookAt(compoRight);
+    spotLight.castShadow = true;
+    scene.add(spotLight);
+
+  
+    document.getElementById('container').appendChild(renderer.domElement);
+
     render();         // render - 음악에 맞는 시각화 모션 불러오기
 
     scene.add(group); // 마지막에 group 을 scene 에 추가
     
 
+
+
+
     // EVENTS
     var saveButton = document.getElementsByClassName("pcr-save");
+    const shape_heart = document.getElementById("shape_heart");
 
     // GEOMETRY
-
+    var now_geometry = '';
     let shape_heart_cnt = 0;
     let button_shape_heart_cnt = 0;
 
 
     // EVENT LISTENERS
     shape_heart.addEventListener("click", ()=>{
-      console.log('하트');
+      shape_heart_cnt += 1
+      console.log('하트가 눌린 갯수', shape_heart_cnt);
       deleteBasics();
-      // containerRender();
+      containerRender();
       createShapeHeart();
       now_geometry = 'shape_heart';
     });
 
-    shape_ring.addEventListener("click", ()=>{
-      console.log('링');
-      deleteBasics();
-      // containerRender();
-      createShapeRing();
-      now_geometry = 'shape_ring';
-    });
 
-    // // save 버튼을 누르면 하트의 색을 바꿔주는 함수
-    // saveButton[0].addEventListener("click", ()=>{
-    //    button_shape_heart_cnt += 1;
-    //    console.log('버튼이 눌린 갯수', button_shape_heart_cnt);
-    //    if (shape_heart_cnt != 0 && shape_heart_cnt % 2 == 1 && now_geometry == 'shape_heart') {
-    //       deleteBasics();
-    //       containerRender();
-    //       createShapeHeart();
-    //       now_geometry = 'shape_heart';
-    //    }
-    // });
+    // save 버튼을 누르면 하트의 색을 바꿔주는 함수
+    saveButton[0].addEventListener("click", ()=>{
+       button_shape_heart_cnt += 1;
+       console.log('버튼이 눌린 갯수', button_shape_heart_cnt);
+       if (shape_heart_cnt != 0 && shape_heart_cnt % 2 == 1 && now_geometry == 'shape_heart') {
+          deleteBasics();
+          containerRender();
+          createShapeHeart();
+          now_geometry = 'shape_heart';
+       }
+    });
     
     // RENDER FUNCTION
-    // function containerRender(){
-    //   container = document.getElementById( "container" );
-    //   renderer = new THREE.WebGLRenderer();
-    //   renderer.setPixelRatio(window.devicePixelRatio);
-    //   renderer.setSize(window.innerWidth, window.innerHeight);
-    //   renderer.toneMapping = THREE.ACESFilmicToneMapping;
-    //   container.appendChild(renderer.domElement);
-    // }
+    function containerRender(){
+      container = document.getElementById( "container" );
+      renderer = new THREE.WebGLRenderer();
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.toneMapping = THREE.ACESFilmicToneMapping;
+      container.appendChild(renderer.domElement);
+    }
 
     // COLOR SAVE & CHANGE FUNCTION
-    // function saveColor(){
-    //   pickr.on('save', (color, instance) => {
-    //     const userColor = color.toHEXA().toString();
-    //     document.querySelector('#userCustom').innerHTML = userColor;
-    //   })
-    // };
+    function saveColor(){
+      pickr.on('save', (color, instance) => {
+        const userColor = color.toHEXA().toString();
+        document.querySelector('#userCustom').innerHTML = userColor;
+      })
+    };
 
-    // function changeColor(){
-    //   var userColor = document.querySelector('#userCustom').innerHTML;
-    //   Material.color = new THREE.Color(userColor);
-    // };
+    function changeColor(){
+      var userColor = document.querySelector('#userCustom').innerHTML;
+      Material.color = new THREE.Color(userColor);
+    };
 
 
 
@@ -349,83 +318,83 @@ var vizInit = function () {
     }
 
 
-    // // DELETE FUNCTION
-    // function deleteBasics(){
-    //   group.remove(compoCenter);
-    //   // group.remove(compoLeft);
-    //   // group.remove(compoRight);
-    // };
+    // DELETE FUNCTION
+    function deleteBasics(){
+      group.remove(compoCenter);
+      // group.remove(compoLeft);
+      // group.remove(compoRight);
+    };
 
 
-    // // UPDATING GEOMETRY FUNCTION
-    // function updateGroupGeometry( mesh, geometry ) {
-    //   mesh.geometry.dispose();
-    //   mesh.geometry = geometry;
-    // }
+    // UPDATING GEOMETRY FUNCTION
+    function updateGroupGeometry( mesh, geometry ) {
+      mesh.geometry.dispose();
+      mesh.geometry = geometry;
+    }
 
 
     // ** CUSTOMIZING BUTTON FUNCTIONS **
     // (1) ShapeGeometry - heart
-  //   function createShapeHeart(){
-  //     // scene = new THREE.Scene();
-  //     // camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 20000);
-  //     // camera.position.set(1, 20, 100);
+    function createShapeHeart(){
+      // scene = new THREE.Scene();
+      camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 20000);
+      camera.position.set(1, 20, 100);
 
-  //     const x = 0;
-  //     const y = 0;
+      const x = 0;
+      const y = 0;
   
-  //     const heartShape = new THREE.Shape();
+      const heartShape = new THREE.Shape();
   
-  //     heartShape.moveTo( x + 5, y + 5 );
-  //     heartShape.bezierCurveTo( x + 5, y + 5, x + 4, y, x, y );
-  //     heartShape.bezierCurveTo( x - 6, y, x - 6, y + 7,x - 6, y + 7 );
-  //     heartShape.bezierCurveTo( x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19 );
-  //     heartShape.bezierCurveTo( x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7 );
-  //     heartShape.bezierCurveTo( x + 16, y + 7, x + 16, y, x + 10, y );
-  //     heartShape.bezierCurveTo( x + 7, y, x + 5, y + 5, x + 5, y + 5 );
+      heartShape.moveTo( x + 5, y + 5 );
+      heartShape.bezierCurveTo( x + 5, y + 5, x + 4, y, x, y );
+      heartShape.bezierCurveTo( x - 6, y, x - 6, y + 7,x - 6, y + 7 );
+      heartShape.bezierCurveTo( x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19 );
+      heartShape.bezierCurveTo( x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7 );
+      heartShape.bezierCurveTo( x + 16, y + 7, x + 16, y, x + 10, y );
+      heartShape.bezierCurveTo( x + 7, y, x + 5, y + 5, x + 5, y + 5 );
   
-  //     var shapeHeartGeometry = new THREE.ShapeGeometry(heartShape);
+      var shapeHeartGeometry = new THREE.ShapeGeometry(heartShape);
       
-  //     var shapeHeartMaterial = new THREE.MeshBasicMaterial({
-  //         color: '#FFFFFF',
-  //         wireframe: false
-  //     });
+      var shapeHeartMaterial = new THREE.MeshBasicMaterial({
+          color: '#FFFFFF',
+          wireframe: false
+      });
 
-  //     // color
-  //     var userColor = document.querySelector('#userCustom').innerHTML;
-  //     shapeHeartMaterial.color = new THREE.Color(userColor);
-  //     changeColorByChroma(shapeHeartMaterial);
+      // color
+      var userColor = document.querySelector('#userCustom').innerHTML;
+      shapeHeartMaterial.color = new THREE.Color(userColor);
+      changeColorByChroma(shapeHeartMaterial);
 
-  //     // mesh position
-  //     var shapeHeartMeshCenter = new THREE.Mesh(shapeHeartGeometry, shapeHeartMaterial);
+      // mesh position
+      var shapeHeartMeshCenter = new THREE.Mesh(shapeHeartGeometry, shapeHeartMaterial);
 
-  //     shapeHeartMeshCenter.position.set(0, 0, 0);
-  //     group.add(shapeHeartMeshCenter);
+      shapeHeartMeshCenter.position.set(0, 0, 0);
+      group.add(shapeHeartMeshCenter);
 
-  //     // light
-  //     var ambientLight = new THREE.AmbientLight(0xaaaaaa);
-  //     scene.add(ambientLight);
+      // light
+      var ambientLight = new THREE.AmbientLight(0xaaaaaa);
+      scene.add(ambientLight);
   
-  //     var spotLight = new THREE.SpotLight(0xffffff);
-  //     spotLight.intensity = 0.9;
-  //     spotLight.position.set(-10, 40, 20);
+      var spotLight = new THREE.SpotLight(0xffffff);
+      spotLight.intensity = 0.9;
+      spotLight.position.set(-10, 40, 20);
       
-  //     spotLight.lookAt(shapeHeartMeshCenter);
-  //     // spotLight.lookAt(shapeHeartMeshLeft);
-  //     // spotLight.lookAt(shapeHeartMeshRight);
-  //     spotLight.castShadow = true;
-  //     scene.add(spotLight);
-  //     scene.add(group);
-
-  //     return shapeHeartMeshCenter
-  // }
+      spotLight.lookAt(shapeHeartMeshCenter);
+      // spotLight.lookAt(shapeHeartMeshLeft);
+      // spotLight.lookAt(shapeHeartMeshRight);
+      spotLight.castShadow = true;
+      scene.add(spotLight);
+      scene.add(group);
+      return shapeHeartMeshCenter
+  }
 
 
     // * 중요 * 렌더링 할 때 시각화 output 의 요소를 바꿔주기
     // * 중요 * 버튼 하나를 클릭할 때마다 렌더링이 된다
     function render() {
       // color rendering
-
+      saveColor();
+      changeColor();
 
       changeColorByChroma(Material);
 
@@ -469,7 +438,6 @@ var vizInit = function () {
           new THREE.RingGeometry(
             13, amplitudeSpectrum[1], 8, 13, 6, maxChroma
         ));
-        // console.log("eee");
         // compoCenter.rotation.z = time * 1.2;
         renderer.render(scene, camera);
         requestAnimationFrame(render);
@@ -478,7 +446,7 @@ var vizInit = function () {
 
     }
   }
-
+}
 
 window.onload = vizInit();
 
