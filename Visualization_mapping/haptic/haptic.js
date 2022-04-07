83b8d0be1555936f018bce679490a8a3892bff2d
TaeHwa Park <dolavvv@naver.com>
2022-04-07 22:38:25 +0900

    add Bluetooth Communication

--- Visualization_mapping/haptic/haptic.js
+++ Visualization_mapping/haptic/haptic.js
@@ -1,10 +1,170 @@
 'use strict';
 
-
 // Create an instance
 var wavesurfer;
-var ch0_data
-var ch1_data
+var ch0_data;
+var ch1_data;
+
+class HapticArray{
+    constructor() {
+        this.devices = []
+      }
+    
+    add(hapticdevice){
+        this.devices.push(hapticdevice);
+    }
+    del(id){
+        let cnt = 0;
+        this.devices.forEach(device => {
+            if (device.device.id == id){
+                this.devices[cnt].disconnect()
+                this.devices.splice(cnt, 1);
+                console.log("device successfully disconneted" );
+            }
+            cnt = cnt + 1;
+        });
+    }
+    send(data){
+        this.devices.forEach(device => {
+            device.writeEbafeaeedccaec(data);
+        });
+    }
+}
+
+var haptic_devices = new HapticArray();
+
+class HapticDevice {
+
+    constructor() {
+      this.device = null;
+      this.onDisconnected = this.onDisconnected.bind(this);
+    }
+    
+    async request() {
+      let options = {
+        "filters": [{
+          "namePrefix": "Haptic"
+        }],
+        "optionalServices": ["6e400001-b5a3-f393-e0a9-e50e24dcca9e"]
+      };
+      this.device = await navigator.bluetooth.requestDevice(options);
+      if (!this.device) {
+        throw "No device selected";
+      }
+      this.device.addEventListener('gattserverdisconnected', this.onDisconnected);
+    }
+    
+    async connect() {
+      if (!this.device) {
+          Promise.reject('Device is not connected.');
+        return 0
+      }
+      await this.device.gatt.connect();
+      return 1
+    }
+    
+    writeEbafeaeedccaec(data) {
+      // RX Characteristic (6E400002-B5A3-F393-E0A9-E50E24DCCA9E)
+      // Write or Write Without Response
+      // Write data to the RX Characteristic to send it on to the UART interface.
+      return this.device.gatt.getPrimaryService("6e400001-b5a3-f393-e0a9-e50e24dcca9e")
+      .then(service => service.getCharacteristic("6e400002-b5a3-f393-e0a9-e50e24dcca9e"))
+      .then(characteristic => characteristic.writeValue(data))
+      .catch(error => {
+        console.log('Argh! ' + error);
+      });
+    }
+  
+    async startNotifications(listener) {
+      // TX Characteristic (6E400003-B5A3-F393-E0A9-E50E24DCCA9E)
+      // Notify
+      // Enable notifications for the TX Characteristic to receive data from the application. The application transmits all data that is received over UART as notifications.
+      const service = await this.device.gatt.getPrimaryService("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
+      const characteristic = await service.getCharacteristic("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
+      await characteristic.startNotifications();
+      characteristic.addEventListener('characteristicvaluechanged', listener);
+    }
+  
+    async stopNotifications(listener) {
+        const service = await this.device.gatt.getPrimaryService("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
+        const characteristic = await service.getCharacteristic("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
+        await characteristic.stopNotifications();
+        characteristic.removeEventListener('characteristicvaluechanged', listener);
+    }
+  
+    disconnect() {
+      if (!this.device) {
+        return Promise.reject('Device is not connected.');
+      }
+      return this.device.gatt.disconnect();
+    }
+  
+    onDisconnected() {
+      console.log('Device is disconnected.');
+    }
+  }
+function onebyte_to_twobyte_int(high, low){
+    let tmp = (((high & 0xff) << 8) | (low & 0xff));
+    return tmp;
+}
+
+function twobyte_int_to_onebyte(origin){
+    let high = ((origin >> 8) & 0xff);
+    let low = origin & 0xff;
+    return [high, low];
+}
+
+function onebyte_to_fourbyte_float(origin){
+    let buf = new ArrayBuffer(4);
+    let view = new DataView(buf);
+    origin.forEach(function (b, i) {
+        view.setInt8(i, b);
+    });
+    return view.getFloat32(0);
+}
+
+function fourbyte_float_to_onebyte(origin){
+    let high = ((origin >> 8) & 0xff);
+    let low = origin & 0xff;
+    return high, low;
+}
+
+function haptic_listener(event){
+    let data_buffer = new ArrayBuffer(19);
+    let view = new Uint8Array(data_buffer);
+
+    for (var i = 0; i<19;i++){
+        view[i] = event.currentTarget.value.getInt8(i);
+    }
+    console.log(view);
+    console.log(data_buffer);
+    // 0xF1 : battery info
+    // 4byte float
+
+    if (view[1]==-15){
+        console.log("current battery" + onebyte_to_fourbyte_float([view[2],view[3],view[4],view[5]])/4.2);
+    }
+    // 0xF2 : echo [freq] [amp]
+    // 2byte, 2byte
+    if (view[1]==-14){
+        console.log("WOW");
+    }
+    // 0xF3 : Device info [BLE Connection Interval MIN] [BLE Connection Interval MAX] [Freq MIN] [Freq MAX]
+    // 2byte, 2byte, 2byte, 2byte
+    if (view[1]==-13){
+        console.log("WOW");
+        console.log("BLE Connection Interval MIN from" + onebyte_to_twobyte_int(view[2],view[3]));
+        console.log("BLE Connection Interval MAX from" + onebyte_to_twobyte_int(0,view[4],view[5]));
+        console.log("Freq MIN" + onebyte_to_twobyte_int(view[6],view[7]));
+        console.log("Freq MAX" + onebyte_to_twobyte_int(view[8],view[9]));
+    }
+    // 0xF4 : Device info2 [firmware version] [hardmware version] [MAC Address]
+    // 2byte, 2byte, 6byte
+    if (view[1]==-12){
+        console.log("WOW");
+    }
+}
+
 // Init & load audio file
 document.addEventListener('DOMContentLoaded', function() {
     // Init
@@ -16,12 +176,61 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Load audio from URL
-    wavesurfer.load('./assets/beat.mp3');
+    wavesurfer.load('./assets/beat2.mp3');
 
     // Log errors
     wavesurfer.on('error', function(msg) {
         console.log(msg);
     });
+    document
+    .querySelector('[data-action="SendHapticPattern"]')
+    .addEventListener('click', function(event) {
+        let data_buffer = new ArrayBuffer(19);
+        let view = new Int8Array(data_buffer);
+        view[0] = 36; // STX 0x24
+        view[1] = 2; // TYPE 0x02
+        
+        var tmp = twobyte_int_to_onebyte(300)
+        view[2] = tmp[0]
+        view[3] = tmp[1]
+        tmp = twobyte_int_to_onebyte(50)
+        view[4] = tmp[0]
+        view[5] = tmp[1]
+        
+        view[17] = 13; // ETX 0x0D
+        view[18] = 10; // ETX 0x0A
+        console.log(view)
+        haptic_devices.send(view);
+        
+    });
+
+    document
+    .querySelector('[data-action="DisconnectHapticDevice"]')
+    .addEventListener('click', function(event) {
+        let device = new HapticDevice();
+        device.request()
+        .then(_=> {
+            haptic_devices.del(device.device.id);
+        })
+        .catch(error => {
+            console.log('Argh! ' + error);
+        });
+    });
+
+    document
+    .querySelector('[data-action="SearchHapticDevice"]')
+    .addEventListener('click', async event => {
+        let device = new HapticDevice();
+        await device.request()
+        let flag = await device.connect();
+        if (flag){
+            await device.startNotifications(haptic_listener)
+            haptic_devices.add(device);
+        }
+        console.log('> Name:             ' + device.device.name);
+        console.log('> Id:               ' + device.device.id);
+        console.log('> Connected:        ' + device.device.gatt.connected);
+        });
 
     // Bind play/pause button
     document
@@ -29,7 +238,7 @@ document.addEventListener('DOMContentLoaded', function() {
         .addEventListener('click', wavesurfer.playPause.bind(wavesurfer));
 
     // document
-    // .querySelector('[data-action="lowfilter"]')
+    // .querySelector('[data-action="lowPassFilter"]')
     // .addEventListener('click', function() {
     //     // load peaks from JSON file. See https://wavesurfer-js.org/faq/
     //     // for instructions on how to generate peaks
