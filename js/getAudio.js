35f9e31781b4f2e8ac8ad23d8282b56b784b2759
TaeHwa Park <dolavvv@naver.com>
2022-04-17 16:21:45 +0900

    fix waveform size

--- js/getAudio.js
+++ js/getAudio.js
@@ -28,11 +28,11 @@ function FileInit() {
         container: document.querySelector('#waveform'),
         waveColor: '#A8DBA8',
         progressColor: '#3B8686',
-        fillParent : true,
+        fillParent: false,
+        minPxPerSec: 4,
         cursorWidth : 5,
         normalize: true
         });
-
 
     var saveButton = document.getElementsByClassName("pcr-save");
     console.log('audio input completed')
