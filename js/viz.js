8a0781d58003593434d165c3365b0d445c2943c0
ChungHaLee <ssk04040@naver.com>
2022-04-19 20:00:44 +0900

    Line Fusion Components added

--- js/viz.js
+++ js/viz.js
@@ -12,9 +12,6 @@ let container, stats;
 let customMenu;
 let FrameRate = 0;
 
-// html buttons (by class name)
-const pitchMenu = document.getElementsByClassName("pitchMenu");
-const dynamicMenu = document.getElementsByClassName("dynamicMenu");
 
 
 // Geometry 별 Detail 요소
@@ -107,7 +104,6 @@ function animate() {
       if (typeof now_geometry == 'number'){
         deleteBasics();
         let [geometry_type, pitch_type, dynamic_type] = GeometryAnalysis(now_geometry);
-        // console.log('GeometryAnalysis: ', [geometry_type, pitch_type, dynamic_type]);
         
         // line custom
         if (geometry_type == 1){
@@ -116,14 +112,28 @@ function animate() {
           } else if (pitch_type == 1){
               if (dynamic_type == 0){
                 createShapeLine_P1D0();
+              } else if (dynamic_type == 5){
+                createShapeLine_P1D5();
+              } else if (dynamic_type == 4){
+                createShapeLine_P1D4();
               }
           } else if (pitch_type == 3){
               if (dynamic_type == 0){
                 createShpaeLine_P3D0();
+              } else if (dynamic_type == 1){
+                createShapeLine_P3D1();
+              } else if (dynamic_type == 4){
+                createShapeLine_P3D4();
+              } else if (dynamic_type == 5){
+                createShapeLine_P3D5();
               }
           } else if (pitch_type == 5){
               if (dynamic_type == 0){
                 createShapeLine_P5D0();
+              } else if (dynamic_type == 1){
+                createShapeLine_P5D1();
+              } else if (dynamic_type == 4){
+                createShapeLine_P5D4();
               }
           } 
           else if (pitch_type == 0){
@@ -198,8 +208,7 @@ function render() {
 }
 
 
-
-
+// =======================================================================
 
 
 // 0. Vanilla geometry
@@ -279,7 +288,7 @@ function createShpaeLine_P3D0(){
 }
 
 function createShapeLine_P5D0(){ 
-  geometry = new THREE.BoxGeometry( 100, 1, 1 );
+  geometry = new THREE.BoxGeometry( 100, 0.5, 1 );
   material = new THREE.MeshBasicMaterial( {
     color: '#FFFFFF',
     wireframe: false
@@ -350,13 +359,188 @@ function createShapeLine_P0D4(){
 }
 
 
+function createShapeLine_P5D1(){
+  let thickness = (maxChroma + 1) * 0.5
+  geometry = new THREE.BoxGeometry( 100, thickness, 1 );
+  material = new THREE.MeshBasicMaterial( {
+    color: '#FFFFFF',
+    wireframe: false
+  } );
+
+  compoCenter = new THREE.Mesh(geometry, material);
+  changeLinePositionbymaxChroma(compoCenter);
+  spotLight.lookAt(compoCenter);
+  group.add( compoCenter );
+  camera.position.set(20, 10, 80);
+}
+
+
+function createShapeLine_P5D4(){
+  geometry = new THREE.BoxGeometry( 100, 1, 1 );
+  material = new THREE.MeshBasicMaterial( {
+    color: '#FFFFFF',
+    wireframe: false
+  } );
+
+  compoCenter = new THREE.Mesh(geometry, material);
+  changeLineColorbymaxChroma(material);
+  changeLinePositionbymaxChroma(compoCenter);
+  spotLight.lookAt(compoCenter);
+  group.add( compoCenter );
+  camera.position.set(20, 10, 80);
+}
+
+
+function createShapeLine_P1D5(){
+  geometry = new THREE.BoxGeometry( maxChroma, energy * 20, 5 );
+  material = new THREE.MeshBasicMaterial( {
+    color: '#FFFFFF',
+    wireframe: false
+  } );
+
+  compoCenter = new THREE.Mesh(geometry, material);
+  compoCenter.position.set(0, -27, 0);
+
+  let compoLeft = new THREE.Mesh(geometry, material);
+  compoLeft.position.set(-20, -30, 0);
+  
+  let compoRight = new THREE.Mesh(geometry, material);
+  compoRight.position.set(20, -30, 0);
+
+  spotLight.lookAt( compoCenter );
+  spotLight.lookAt( compoLeft );
+  spotLight.lookAt( compoRight );
+  group.add( compoCenter );
+  group.add( compoLeft );
+  group.add( compoRight );
+  camera.position.set(1, -5, 100);
+}
+
+
+function createShapeLine_P1D4(){
+  geometry = new THREE.BoxGeometry( 100, maxChroma * 1.5, 1 );
+  material = new THREE.MeshBasicMaterial( {
+    color: '#FFFFFF',
+    wireframe: false
+  } );
+
+  changeLineColorbymaxChroma(material);
+
+  compoCenter = new THREE.Mesh(geometry, material);
+  spotLight.lookAt(compoCenter);
+  group.add( compoCenter );
+  camera.position.set(20, 0, 50);
+}
+
+function createShapeLine_P3D1(){
+  class CustomSinCurve extends THREE.Curve {
+    constructor( scale = 1 ) {
+      super();
+      this.scale = scale;
+    }
+  
+    getPoint( t, optionalTarget = new THREE.Vector3() ) {
+      const tx = t * 3 - 1.5;
+      const ty = Math.sin( 10 * Math.PI * t );
+      const tz = 0;
+      return optionalTarget.set( tx, ty, tz ).multiplyScalar( this.scale );
+    }
+  }
+
+  const path = new CustomSinCurve( 40 );
+  let custom_maxChroma = maxChroma + 1;
+  let thickness = energy * 5;
+  geometry = new THREE.TubeGeometry( path, custom_maxChroma, thickness, custom_maxChroma, false );
+
+  material = new THREE.MeshBasicMaterial( { 
+    color: '#FFFFFF',
+    wireframe: false
+  } );
+
+  compoCenter = new THREE.Mesh( geometry, material );
+  compoCenter.position.set(0, 0, 0);
+  spotLight.lookAt(compoCenter);
+  group.add( compoCenter );
+  camera.position.set(0, 0, 130);
+}
+
+
+function createShapeLine_P3D4(){
+  class CustomSinCurve extends THREE.Curve {
+    constructor( scale = 1 ) {
+      super();
+      this.scale = scale;
+    }
+  
+    getPoint( t, optionalTarget = new THREE.Vector3() ) {
+      const tx = t * 3 - 1.5;
+      const ty = Math.sin( 10 * Math.PI * t );
+      const tz = 0;
+      return optionalTarget.set( tx, ty, tz ).multiplyScalar( this.scale );
+    }
+  }
+
+  const path = new CustomSinCurve( 40 );
+  let custom_maxChroma = maxChroma + 1
+  geometry = new THREE.TubeGeometry( path, custom_maxChroma, 2, custom_maxChroma, false );
+
+  material = new THREE.MeshBasicMaterial( { 
+    color: '#FFFFFF',
+    wireframe: false
+  } );
+
+  changeLineColorbymaxChroma(material);
+  compoCenter = new THREE.Mesh( geometry, material );
+  compoCenter.position.set(0, 0, 0);
+  spotLight.lookAt(compoCenter);
+  group.add( compoCenter );
+  camera.position.set(0, 0, 130);
+}
+
+function createShapeLine_P3D5(){
+  class CustomSinCurve extends THREE.Curve {
+    constructor( scale = 1 ) {
+      super();
+      this.scale = scale;
+    }
+  
+    getPoint( t, optionalTarget = new THREE.Vector3() ) {
+      const tx = t * 3 - 1.5;
+      const ty = Math.sin( 10 * Math.PI * t );
+      const tz = 0;
+      return optionalTarget.set( tx, ty, tz ).multiplyScalar( this.scale );
+    }
+  }
+
+  const path = new CustomSinCurve( 40 );
+  let custom_maxChroma = maxChroma + 1
+  let height = energy * 10
+  console.log('위치(높이)', height)
+  geometry = new THREE.TubeGeometry( path, custom_maxChroma, 2, custom_maxChroma, false );
+
+  material = new THREE.MeshBasicMaterial( { 
+    color: '#FFFFFF',
+    wireframe: false
+  } );
+
+  compoCenter = new THREE.Mesh( geometry, material );
+  compoCenter.position.set(0, height, 0);
+  spotLight.lookAt(compoCenter);
+  group.add( compoCenter );
+  camera.position.set(0, 0, 130);
+}
+
+
+
+// =======================================================================
+
+
 // 3. Ring geometry
-// =======
 function createShapeRing_P0D1(){
   let thickness = energy*5+1;
   let inner = 13 - 13/thickness
-  // console.log(thickness);
-  geometry = new THREE.RingGeometry(inner, 13, 8, 13, 0, 6.283185307179586);
+
+  geometry = new THREE.RingGeometry(inner, 13, 8, 13, 0, 6.283);
   material = new THREE.MeshLambertMaterial({
     color: '#FFFFFF',
     wireframe: true,
@@ -366,10 +550,8 @@ function createShapeRing_P0D1(){
   compoCenter.position.set(1, 10, 0);
   spotLight.lookAt(compoCenter);
 
-  // group = new THREE.Group();
   group.add( compoCenter );
   camera.position.set(1, 10, 70);
-  // scene.add( group );
 }
 
 function createShapeRing_P0D2(){
@@ -381,7 +563,7 @@ function createShapeRing_P0D2(){
   }
 
   let size = custom_energy
-  geometry = new THREE.RingGeometry(size-3, size, 8, 13, 0, 6.283185307179586);
+  geometry = new THREE.RingGeometry(size-3, size, 8, 13, 0, 6.283);
   material = new THREE.MeshLambertMaterial({
     color: '#FFFFFF',
     wireframe: true,
@@ -391,17 +573,14 @@ function createShapeRing_P0D2(){
   compoCenter.position.set(1, 10, 0);
   spotLight.lookAt(compoCenter);
 
-  // group = new THREE.Group();
   group.add( compoCenter );
   camera.position.set(1, 10, 70);
-  // scene.add( group );
 }
 
 function createShapeRing_P0D3(){
   let segments = (Math.round(energy)+3);
 
-  // console.log(energy);
-  geometry = new THREE.RingGeometry(10, 13, segments, 13, 0, 6.283185307179586);
+  geometry = new THREE.RingGeometry(10, 13, segments, 13, 0, 6.283);
   material = new THREE.MeshLambertMaterial({
     color: '#FFFFFF',
     wireframe: true,
@@ -411,16 +590,14 @@ function createShapeRing_P0D3(){
   compoCenter.position.set(1, 10, 0);
   spotLight.lookAt(compoCenter);
 
-  // group = new THREE.Group();
   group.add( compoCenter );
   camera.position.set(1, 10, 70);
-  // scene.add( group );
 }
 
 // >>>>>>> 547641bbc724e13bf499baf9a4e6eb3b521b2734
 function createShapeRing_P1D0(){
   let thickness = maxChroma/2+5
-  geometry = new THREE.RingGeometry(thickness, 13, 8, 13, 0, 6.283185307179586);
+  geometry = new THREE.RingGeometry(thickness, 13, 8, 13, 0, 6.283);
   material = new THREE.MeshLambertMaterial({
     color: '#FFFFFF',
     wireframe: true,
@@ -430,10 +607,8 @@ function createShapeRing_P1D0(){
   compoCenter.position.set(1, 10, 0);
   spotLight.lookAt(compoCenter);
 
-  // group = new THREE.Group();
   group.add( compoCenter );
   camera.position.set(1, 10, 70);
-  // scene.add( group );
 }
 
 function createShapeRing_P1D1(){
@@ -455,10 +630,8 @@ function createShapeRing_P1D1(){
   compoCenter.position.set(1, 10, 0);
   spotLight.lookAt(compoCenter);
 
-  // group = new THREE.Group();
   group.add( compoCenter );
   camera.position.set(1, 10, 70);
-  // scene.add( group );
 }
 
 function createShapeRing_P1D2(){
@@ -494,7 +667,7 @@ function createShapeRing_P1D3(){
   let thickness = maxChroma+2;
   let segments = custom_energy
   let inner = 13 - 13/thickness
-  geometry = new THREE.RingGeometry(inner, 13, segments, 13, 0, 6.283185307179586);
+  geometry = new THREE.RingGeometry(inner, 13, segments, 13, 0, 6.283);
   material = new THREE.MeshLambertMaterial({
     color: '#FFFFFF',
     wireframe: true,
@@ -504,17 +677,15 @@ function createShapeRing_P1D3(){
   compoCenter.position.set(1, 10, 0);
   spotLight.lookAt(compoCenter);
 
-  // group = new THREE.Group();
   group.add( compoCenter );
   camera.position.set(1, 10, 70);
-  // scene.add( group );
 }
 
 
 function createShapeRing_P2D0(){
 
   let size = (maxChroma/2+5);
-  geometry = new THREE.RingGeometry(size-3, size, 8, 13, 0, 6.283185307179586);
+  geometry = new THREE.RingGeometry(size-3, size, 8, 13, 0, 6.283);
   material = new THREE.MeshLambertMaterial({
     color: '#FFFFFF',
     wireframe: true,
@@ -544,10 +715,8 @@ function createShapeRing_P2D1(){
   compoCenter.position.set(1, 10, 0);
   spotLight.lookAt(compoCenter);
 
-  // group = new THREE.Group();
   group.add( compoCenter );
   camera.position.set(1, 10, 70);
-  // scene.add( group );
 }
 
 function createShapeRing_P2D2(){
@@ -558,7 +727,7 @@ function createShapeRing_P2D2(){
     custom_energy = custom_energy/2+5
   }
   let size = ((maxChroma/2+5)+custom_energy)/2;
-  geometry = new THREE.RingGeometry(size-3, size, 8, 13, 0, 6.283185307179586);
+  geometry = new THREE.RingGeometry(size-3, size, 8, 13, 0, 6.283);
   material = new THREE.MeshLambertMaterial({
     color: '#FFFFFF',
     wireframe: true,
@@ -568,10 +737,8 @@ function createShapeRing_P2D2(){
   compoCenter.position.set(1, 10, 0);
   spotLight.lookAt(compoCenter);
 
-  // group = new THREE.Group();
   group.add( compoCenter );
   camera.position.set(1, 10, 70);
-  // scene.add( group );
 }
 
 function createShapeRing_P2D3(){
@@ -581,8 +748,7 @@ function createShapeRing_P2D3(){
     custom_energy = custom_energy+3;
   }
   let segments = custom_energy
-  // console.log(energy);
-  geometry = new THREE.RingGeometry(size-3, size, segments, 13, 0, 6.283185307179586);
+  geometry = new THREE.RingGeometry(size-3, size, segments, 13, 0, 6.283);
   material = new THREE.MeshLambertMaterial({
     color: '#FFFFFF',
     wireframe: true,
@@ -601,7 +767,7 @@ function createShapeRing_P2D3(){
 function createShapeRing_P3D0(){
   let segments = (maxChroma+3);
 
-  geometry = new THREE.RingGeometry(10, 13, segments, 13, 0, 6.283185307179586);
+  geometry = new THREE.RingGeometry(10, 13, segments, 13, 0, 6.283);
   material = new THREE.MeshLambertMaterial({
     color: '#FFFFFF',
     wireframe: true,
@@ -611,18 +777,16 @@ function createShapeRing_P3D0(){
   compoCenter.position.set(1, 10, 0);
   spotLight.lookAt(compoCenter);
 
-  // group = new THREE.Group();
   group.add( compoCenter );
   camera.position.set(1, 10, 70);
-  // scene.add( group );
 }
 
 function createShapeRing_P3D1(){
   let segments = (maxChroma+3);
   let custom_energy = Math.round(energy*5)+2;
   let inner = 13 - 13/custom_energy
-  // console.log(energy);
-  geometry = new THREE.RingGeometry(inner, 13, segments, 13, 0, 6.283185307179586);
+
+  geometry = new THREE.RingGeometry(inner, 13, segments, 13, 0, 6.283);
   material = new THREE.MeshLambertMaterial({
     color: '#FFFFFF',
     wireframe: true,
@@ -632,24 +796,20 @@ function createShapeRing_P3D1(){
   compoCenter.position.set(1, 10, 0);
   spotLight.lookAt(compoCenter);
 
-  // group = new THREE.Group();
   group.add( compoCenter );
   camera.position.set(1, 10, 70);
-  // scene.add( group );
 }
 
 function createShapeRing_P3D2(){
-  var segments = (maxChroma+3);
-
-  var custom_energy = energy*5;
+  let segments = (maxChroma+3);
+  let custom_energy = energy*5;
   if(custom_energy>15){
     custom_energy = 15;
   } else if(custom_energy<10){
     custom_energy = custom_energy/2+5
   }
-    // console.log(energy);
   let size = custom_energy
-  geometry = new THREE.RingGeometry(size-3, size, segments, 13, 0, 6.283185307179586);
+  geometry = new THREE.RingGeometry(size-3, size, segments, 13, 0, 6.283);
   material = new THREE.MeshLambertMaterial({
     color: '#FFFFFF',
     wireframe: true,
@@ -659,20 +819,15 @@ function createShapeRing_P3D2(){
   compoCenter.position.set(1, 10, 0);
   spotLight.lookAt(compoCenter);
 
-  // group = new THREE.Group();
   group.add( compoCenter );
   camera.position.set(1, 10, 70);
-  // scene.add( group );
 }
 
 function createShapeRing_P3D3(){
-
   let custom_energy = energy*5;
-  let segments = Math.round((maxChroma+custom_energy)/2+3)
-  // console.log(segments);
+  let segments = Math.round((maxChroma+custom_energy)/2+3);
 
-    // console.log(energy);
-  geometry = new THREE.RingGeometry(10, 13, segments, 13, 0, 6.283185307179586);
+  geometry = new THREE.RingGeometry(10, 13, segments, 13, 0, 6.283);
   material = new THREE.MeshLambertMaterial({
     color: '#FFFFFF',
     wireframe: true,
@@ -682,10 +837,8 @@ function createShapeRing_P3D3(){
   compoCenter.position.set(1, 10, 0);
   spotLight.lookAt(compoCenter);
 
-  // group = new THREE.Group();
   group.add( compoCenter );
   camera.position.set(1, 10, 70);
-  // scene.add( group );
 }
 
 function deleteBasics(){
@@ -705,9 +858,7 @@ function updateGroupGeometry( mesh, geometry ) {
 }
 
 
-
-
-
+// =======================================================================
 
 
 function vizInit() {
@@ -988,21 +1139,20 @@ function changeBGColor(){
 }
 
 
-
 function changeLineColorbymaxChroma(Material){
   // color rendering by pitch
-  let plainRed = new THREE.Color('rgba(247, 152, 152)');
-  let plainRedOrange = new THREE.Color('rgba(247, 189, 152)');
-  let plainOrange = new THREE.Color('rgba(247, 206, 152)');
-  let plainOrangeYellow = new THREE.Color('rgba(205, 159, 59)');
-  let plainYellow = new THREE.Color('rgba(247, 233, 152)');
-  let plainYeondu = new THREE.Color('rgba(203, 247, 152)');
-  let plainGreen = new THREE.Color('rgba(152, 247, 177)');
-  let plainCyan = new THREE.Color('rgba(152, 246, 247)');
-  let plainBlue = new THREE.Color('rgba(152, 179, 247)');
-  let plainViolet = new THREE.Color('rgba(178, 152, 247)');
-  let plainMagenta = new THREE.Color('rgba(241, 152, 247)');
-  let plainPink = new THREE.Color('rgba(247, 152, 213)');
+  let plainRed = new THREE.Color('rgba(255, 31, 15)');
+  let plainRedOrange = new THREE.Color('rgba(255, 114, 15)');
+  let plainOrange = new THREE.Color('rgba(255, 153, 0)');
+  let plainOrangeYellow = new THREE.Color('rgba(255, 200, 0)');
+  let plainYellow = new THREE.Color('rgba(255, 229, 0)');
+  let plainYeondu = new THREE.Color('rgba(166, 255, 0)');
+  let plainGreen = new THREE.Color('rgba(0, 255, 25)');
+  let plainCyan = new THREE.Color('rgba(0, 255, 210)');
+  let plainBlue = new THREE.Color('rgba(0, 132, 255)');
+  let plainViolet = new THREE.Color('rgba(83, 0, 255)');
+  let plainMagenta = new THREE.Color('rgba(255, 0, 248)');
+  let plainPink = new THREE.Color('rgba(255, 0, 152)');
 
   if (maxChroma == 0){ 
     Material.color = plainRed;
@@ -1034,29 +1184,29 @@ function changeLineColorbymaxChroma(Material){
 
 function changeLinePositionbymaxChroma(mesh){
   if (maxChroma == 0){ 
-    mesh.position.set(30, -40, 10);
+    mesh.position.set(30, -2, 10);
   } else if (maxChroma == 1) {
-    mesh.position.set(30, -35, 10);
-  } else if (maxChroma == 2){
-    mesh.position.set(30, -30, 10);
-  } else if (maxChroma == 3){
-    mesh.position.set(30, -25, 10);
-  } else if (maxChroma == 4){
-    mesh.position.set(30, -20, 10);
-  } else if (maxChroma == 5){
-    mesh.position.set(30, -15, 10);
-  } else if (maxChroma == 6){
-    mesh.position.set(30, -10, 10);
-  } else if (maxChroma == 7){
-    mesh.position.set(30, -5, 10);
-  } else if (maxChroma == 8){
     mesh.position.set(30, 0, 10);
-  } else if (maxChroma == 9){
-    mesh.position.set(30, 5, 10);
-  } else if (maxChroma == 10){
+  } else if (maxChroma == 2){
+    mesh.position.set(30, 2, 10);
+  } else if (maxChroma == 3){
+    mesh.position.set(30, 4, 10);
+  } else if (maxChroma == 4){
+    mesh.position.set(30, 6, 10);
+  } else if (maxChroma == 5){
+    mesh.position.set(30, 8, 10);
+  } else if (maxChroma == 6){
     mesh.position.set(30, 10, 10);
+  } else if (maxChroma == 7){
+    mesh.position.set(30, 12, 10);
+  } else if (maxChroma == 8){
+    mesh.position.set(30, 14, 10);
+  } else if (maxChroma == 9){
+    mesh.position.set(30, 16, 10);
+  } else if (maxChroma == 10){
+    mesh.position.set(30, 18, 10);
   } else {
-    mesh.position.set(30, 15, 10);
+    mesh.position.set(30, 20, 10);
   }
 }
 
