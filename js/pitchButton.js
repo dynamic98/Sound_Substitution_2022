479828598b374f2e3ffd8ea93a8a32d2e44f6271
ChungHaLee <ssk04040@naver.com>
2022-04-21 12:19:48 +0900

    solve error please

--- js/pitchButton.js
+++ js/pitchButton.js
@@ -10,64 +10,51 @@ const pitch_si = document.getElementById("pitch_si");
 
 let audioTitle
 let file, audio, fileLabel, audio_context;
-let realTitle = document.getElementById('title');
+
 let analyser, wavesurfer, src, bufferLength, dataArray;
 let chroma, maxChroma, energy, amplitudeSpectrum;
 let AudioLastTime, AudioCurrentTime;
 
 let audioPlayer = document.getElementById("player");
 let clickCount = 0;
+let isPlaying = false;
 
 
 
-
-// REAL ONE !
 function FileInit(pitchButton) {
     let pitch = String(pitchButton.id);
     let audioPath = `./pitch_audio/${pitch}.mp3`
+
+
     audio = new Audio(audioPath);
     audio_context = audio_context || new AudioContext();
+    audio.load();
+    readyWavesurfer(pitchButton);
+    eventListenPitch(pitchButton);
 
-    wavesurfer = WaveSurfer.create({
-        container: document.querySelector('#waveform'),
-        waveColor: '#A8DBA8',
-        progressColor: '#3B8686',
-        cursorWidth : 5,
-        normalize: true,
-        });
-    
     document
     .querySelector(`#${pitch}`)
-    .addEventListener('click', ()=>{ 
-        console.log("PlayPause button pressed");
-        wavesurfer.playPause();
+    .addEventListener('click', ()=>{
 
-        TogglePlay();
+        if (audio.paused){
+            audio.play();
+            wavesurfer.play();
+        } else {
+            audio.pause();
+            wavesurfer.pause();
+        }
     })
 };
+;
 
 
-// 알맞은 버튼 요소를 넣으면 오디오 실행해주는 함수.
+
+
+// 버튼 색상 변경해주는 함수
 function eventListenPitch(pitchButton){
     pitchButton.addEventListener("click", ()=>{
         clickCount = clickCount + 1
-        let pitch = String(pitchButton.id);
-        let audioPath = `./pitch_audio/${pitch}.mp3` // 함수화 시키기위해 문자열 포매팅
-        audio = new Audio(audioPath);
-        wavesurfer.load(audio);
-        audio.load();
-        src = audio_context.createMediaElementSource(audio);
 
-        wavesurfer.on('ready', () => {
-            console.log("wavesurfer is ready");
-            audio.play();
-            wavesurfer.play();
-
-            audio.volume = 0.2;
-        })
-        AnalyzerPlay(src);
-
-        // 버튼 색깔 바꾸기
         if (clickCount % 2 != 0){
             pitchButton.style.background = '#FF5C5C';
         } else {
@@ -76,7 +63,7 @@ function eventListenPitch(pitchButton){
     });
 }
 
-
+// meyda analyzer 실행 함수
 function AnalyzerPlay(src) {
     console.log("AnalyzerPlay starts");
     analyser = audio_context.createAnalyser();
@@ -110,6 +97,33 @@ function AnalyzerPlay(src) {
     meyda_analyser.start();
 }
 
+
+// wavesurfer 가져오는 함수
+function readyWavesurfer(pitchButton){
+    wavesurfer = WaveSurfer.create({
+        container: document.querySelector('#waveform'),
+        waveColor: '#A8DBA8',
+        progressColor: '#3B8686',
+        cursorWidth : 5,
+        normalize: true,
+        });
+
+    let pitch = String(pitchButton.id);
+    let audioPath = `./pitch_audio/${pitch}.mp3` // 함수화 시키기위해 문자열 포매팅
+    audio = new Audio(audioPath);
+    wavesurfer.load(audio);
+    // audio.load();
+    src = audio_context.createMediaElementSource(audio);
+
+    wavesurfer.on('ready', () => {
+        console.log("wavesurfer is ready");
+        audio.volume = 0.2;
+    })
+
+    AnalyzerPlay(src);
+}
+
+
 // 여기에서 싱크가 잘 안맞는듯?
 function SyncAudio(){
     wavesurfer.on('audioprocess', function() {
@@ -129,16 +143,5 @@ function SyncAudio(){
 
 
 
-function TogglePlay(){
-    if (audio.paused){
-        audio.play();
-    } else {
-        audio.pause();
-    }
-}
-
-
-
 FileInit(pitch_do);
-eventListenPitch(pitch_do);
-SyncAudio();
+// SyncAudio();
