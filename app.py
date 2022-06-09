893b497c9b837a9c6ce87914f9909ad384fbe62e
ChungHaLee <ssk04040@naver.com>
2022-06-09 20:53:48 +0900

    changed music directory

--- app.py
+++ app.py
@@ -3,31 +3,31 @@ from featureExtraction import extract_feature
 from werkzeug.utils import secure_filename
 import os
 
+
 app = Flask(__name__)
+folder = os.path.join('static/music')
+
+
+os.makedirs(folder, exist_ok=True)
+
 
 @app.route('/')
 def index():
   return render_template('mainMenu.html')
 
+
 # abstact 페이지에서는 오디오 파일을 변수에 저장만 하기
 @app.route('/abstract', methods=['POST'])
 def abstract():
-  return render_template('abstract.html')
-  
-
-
-# abstracted 페이지에서는 저장된 오디오 파일의 변수를 html 에 전달
-@app.route('/abstracted', methods=['POST'])
-def abstracted():
   if request.method == 'POST':
       file = request.files['thefile']
-      print('file:', file)
-      file.save(secure_filename(file.filename))
+      file.save(os.path.join(folder, secure_filename(file.filename)))
       print('file has saved!')
+      return render_template('abstract.html')
   else:
-    pass
-  return render_template('abstract.html')
-
+    print('file was not saved!')
+    return render_template('mainMenu.html')
+  
 
 
 
