e42d8baf3c8d64e1e36b2d19a0f546c138282dc3
syeminPark <soccer0874@gmail.com>
2022-07-05 19:19:08 +0900

    주석달기

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,6 @@
 // import {analyser, chroma, maxChroma, energy, amplitudeSpectrum, dataArray, bufferLength, audio, wavesurfer, show_canvas, audio_context, src } from './modules.js';
 
+//원래 코드로 실행하려면 이 밑의 전부 주석처리를 하고 윗 줄 하나의 주석을 풀먼 된다. 
 import{FileHandler} from '../customjs/FileHandler.js';
 import{MyWaveSurfer} from '../customjs/MyWaveSurfer.js';
 import{MyAudioContext} from '../customjs/MyAudioContext.js';
