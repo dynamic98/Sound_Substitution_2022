a43308f6b2c9eb7690115a285b49248ab7b60160
dynamic98 <dynamic98@gist.ac.kr>
2022-07-14 17:27:33 +0900

    haptic customization is done

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: {
         abstract: path.resolve(__dirname, "customJS", "main.js"),
         //이런식으로 추가하면 된다 파일 이름을. 그러면 dist 폴더에 concrete.js 가 만들어진다. 위의 경우 abstract.js이 만들어진다. 
-        concrete: path.resolve(__dirname, "customJS", "concrete.js")
+        concrete: path.resolve(__dirname, "customJS", "concrete.js"),
     },
     mode: "development",
     target: "web",
