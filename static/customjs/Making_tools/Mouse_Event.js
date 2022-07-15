07e491809eada16e54dd096c49e6fdec88cccb78
Yeo-Gyeong Noh <ygnoh0210@gmail.com>
2022-07-15 20:36:45 +0900

    color magnititude added

--- static/customjs/Making_tools/Mouse_Event.js
+++ static/customjs/Making_tools/Mouse_Event.js
@@ -1,9 +1,21 @@
 
 let selecting = false
 let selected_className=''
+let time=32
+let pitch=24
+let melody_canvas = new Array(time)
 
 const tp_grid = document.getElementById('full_grid')
-
+init()
+function init(){
+    for (var i=0; i<time; i++){
+        melody_canvas[i]=new Array(pitch)
+        for (var j=0; j<pitch; j++){
+            // melody_canvas[i][j]=[0]
+            melody_canvas[i][j]=[parseInt(0)]
+        }
+    }
+}
 
 tp_grid.onclick=function(e){
     selected_className = e.path[0].className
@@ -29,75 +41,36 @@ function logging(cord){
         var splited_cord = cord.split('_')
         var t_position= splited_cord[0].replace('t', '')
         var p_position=splited_cord[1].replace('p', '')
-        // console.log(t_position, p_position)
-        color_change(cord, t_position, p_position)
+        melody_canvas[t_position-1][p_position-1]=parseInt(melody_canvas[t_position-1][p_position-1])+1
+        // color_change(cord, t_position, p_position)
     }
+    set_color()
 }
 
 function color_change(tar_class_name, t, p){
     var el = document.getElementsByClassName(tar_class_name);
     let ContainerName = document.getElementsByClassName('container');
+    // console.log(tar_class_name, t, p)
     // console.log(el[0]);
     if(el!=ContainerName){
-        var color=(t*30).toString()
-        // console.log(color
-        console.log(p, t)
-        // el[0].style.background = 'blue'
-        // hsl
-        el[0].style.background= 'hsl('+color+', 70%, 40%)';
-        // color:hsl(0, 70%, 40%)
+        var color=(p*30).toString()
+        el[0].style.background= 'hsl('+color+', 50%, 70%)';
+
     }
 }
 
-// document.onclick=function(e){
-//     var class_name_is = e.class_name
-//     console.log(class_name_is)
-// }
+function set_color(){
+    for (var i=0; i<time; i++){
+        for(var j=0; j<pitch; j++){
+            let el_classname="t"+(i+1).toString()+"_p"+(j+1).toString()
+            let el =document.getElementsByClassName(el_classname)
+            var color=(j*30).toString()
+            var satuation=melody_canvas[i][j]*35
+            if(melody_canvas[i][j]!=0){
+                el[0].style.background='hsl('+color+', '+satuation+'%, 70%)';
+                console.log(color, satuation, melody_canvas)
+            }
+        }
+    }
 
-// function tp_cord(event){
-//     // console.log("(x, y) : ", x, y)
-//     var class_name = event.class_name;
-//     this.class_name
-//     console.log(class_name)
-// }
-
-// const full_grid_canvas = document.getElementById('full_grid');
-
-// full_grid_canvas.onmousedown = function(e){
-//     selecting=true
-//     let x = e.offsetX
-//     let y = e.offsetY
-//     console.log(x, y)
-// }
-
-// full_grid_canvas.onmouseup = function(e){
-//     selecting=false
-// }
-
-// full_grid_canvas.onmousemove = function(e){
-//     if (selecting==true){
-//         let x = e.offsetX
-//         let y = e.offsetY
-//         console.log(x, y)
-//     }
-
-// }
-
-
-// full_grid_canvas.addEventListener('mousedown', (GetGridPosition) => {
-//     selecting = true
-//     console.log("mousedown")
-//     GetGridPosition();
-// });
-
-// full_grid_canvas.addEventListener('mousemove', (event)=>{
-//     if (selecting==true){
-//         console.log("selecting")
-//     }
-// });
-
-// full_grid_canvas.addEventListener('mouseup', (event) => {
-//     console.log("mouseup")
-//     selecting = false
-// });
-
+}
