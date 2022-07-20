486c3a6858bf799d25a1363fb82b3ae46e67591a
Yeo-Gyeong Noh <ygnoh0210@gmail.com>
2022-07-20 18:23:10 +0900

    making_tool_v1 completed

--- static/customjs/Making_tools/Mouse_Event.js
+++ static/customjs/Making_tools/Mouse_Event.js
@@ -1,4 +1,5 @@
 // import * as Tone from "../";
+
 let selecting = false
 let selected_className=''
 let time=32
@@ -6,15 +7,23 @@ let pitch=24
 let speed=2
 let melody_canvas = new Array(time)
 let painting_mode= true
+let note=16
+let beat=3
 
-let instrument=["piano", "guitar", "marimba"]
-let tool=["restart", "paint", "erase", "save"]
-let playcontrol=["play", "stop"]
+// let instrument=["piano", "guitar", "marimba"]
+let emotion=["happy", "sad", "exciting", "peaceful"]
+let palette=["palette1", "palette2", "palette3", "palette4", "palette5", "palette6"]
+let shape=["shape1", "shape2", "shape3", "shape4"]
+let tool=["restart", "paint", "erase"]
+let playcontrol=["play", "stop", "save"]
 let pitch_dict={1:"C4", 2:"C#4",3:"D4",4:"D#4",5:"E4",6:"F4",7:"F#4",8:"G4",9:"G#4",10:"A4",11:"A#4",12:"B4",
                 13:"C5", 14:"C#5",15:"D5",16:"D#5",17:"E5",18:"F5",19:"F#5",20:"G5",21:"G#5",22:"A5",23:"A#5",24:"B5"};
 
 const tp_grid = document.getElementById('full_grid')
 const synth = new Tone.Synth().toDestination();
+const drum = new Tone.Synth().toDestination();
+// const piano_inst=new Tone.Piano().toDestination();
+
 init();
 function init(){
     // Tone.start()
@@ -22,10 +31,12 @@ function init(){
         melody_canvas[i]=new Array(pitch)
         for (var j=0; j<pitch; j++){
             // melody_canvas[i][j]=[0]
-            melody_canvas[i][j]=[parseInt(0)]
+            melody_canvas[i][j]=parseInt(0)
             let el_classname="t"+(i+1).toString()+"_p"+(j+1).toString()
             let el =document.getElementsByClassName(el_classname)
             el[0].style.background="black"
+            el[0].style.border="dotted"
+            el[0].style.borderWidth ="0.01em"
         }
     }
 }
@@ -56,8 +67,10 @@ tp_grid.onmousedown=function(e){
     }
 }
 tp_grid.onmousemove=function(e){
+   
     selected_className = e.path[0].className
-    if((selecting==true)&&(selected_className.includes('_')&&(selected_className!="menu_left")&&(selected_className!="menu_right"))){
+    if((selecting==true)&&(selected_className.includes('_')&&(selected_className!="menu_left")&&(selected_className!="menu_right"&&(selected_className!="blank_bottom")))){
+        // console.log(selected_className, e.path[0].id)
         logging(selected_className)
     }
 }
@@ -72,10 +85,19 @@ function logging(cord){
         var t_position= splited_cord[0].replace('t', '')
         var p_position=splited_cord[1].replace('p', '')
         if (painting_mode==true){
-            melody_canvas[t_position-1][p_position-1]=parseInt(melody_canvas[t_position-1][p_position-1])+1}
+            for(let i=0; i<24; i++){
+                if(i==parseInt(p_position-1)){
+                    continue;
+                }
+                if(melody_canvas[parseInt(t_position)-1][i]!=0){
+                    melody_canvas[parseInt(t_position)-1][i]=0
+                }
+            }
+            melody_canvas[t_position-1][p_position-1]=parseInt(melody_canvas[t_position-1][p_position-1])+1
+        }
         else if(painting_mode==false){
             melody_canvas[t_position-1][p_position-1]=parseInt(0)}
-        console.log(cord)
+        // console.log(cord, melody_canvas[t_position-1][p_position-1])
         // color_change(cord, t_position, p_position)
     }
     set_color()
@@ -100,7 +122,8 @@ function set_color(){
             let el_classname="t"+(i+1).toString()+"_p"+(j+1).toString()
             let el =document.getElementsByClassName(el_classname)
             var color=(j*30).toString()
-            var satuation=melody_canvas[i][j]*35
+            var satuation='100'
+            // var satuation=melody_canvas[i][j]*35
             if(melody_canvas[i][j]!=0){
                 el[0].style.background='hsl('+color+', '+satuation+'%, 70%)';
                 // console.log(color, satuation, melody_canvas)
@@ -117,10 +140,8 @@ function set_instrument(inst){
 }
 
 function set_tool(tool){
-    // console.log(tool)
-
     if (tool=="paint"){
-        let mode_button=document.getElementById('paint')
+        // let mode_button=document.getElementById('paint')
         // mode_button.style.animation='blink'
         painting_mode=true
     }
@@ -129,39 +150,54 @@ function set_tool(tool){
     }
     if (tool=="restart"){
         init()
-        console.log(melody_canvas)
+        // console.log(melody_canvas)
         set_color()
     }
 }
 
 
 function playPiano(time, pitch_num, counter){
-    
+    if (counter=="initial"){
+        Tone.start()
+    }
+        Tone.start()
+        let now=Tone.now()
+        synth.triggerAttackRelease(pitch_dict[pitch_num], note.toString()+'n', now+time/5)
+}
+
+function playDrum(time, type){
     Tone.start()
     let now=Tone.now()
-    console.log("----------------------------------------------")
-    console.log(time, pitch_num, pitch_dict[pitch_num], counter)
-    // synth.triggerAttack(pitch_dict[pitch_num, now], time, counter)
-    // synth.triggerRelease(now+100)
-    synth.volume.value=counter*10
-    synth.triggerAttackRelease(pitch_dict[pitch_num], '2n', now+time/10)
-    // console.log(time, pitch_num, counter)
-    // console.log(pitch_dict[pitch_num], now+time/10)
-    
+    let drum_pitch="C2"
+    if (type==2){
+        drum_pitch="E2"        
+    }
+    drum.triggerAttackRelease(drum_pitch, note.toString()+'n', now+time/5)
 }
 
 function set_playcontrol(play_controller){
-    console.log(play_controller)
     if(play_controller=="play"){
-
+        console.log(melody_canvas)
+        playPiano(0, 0, "initial")
         for(var i=0; i<time; i++){
             let current_melody=melody_canvas[i]
+            if(i%beat==0){
+                playDrum(i, 1)
+            }else{
+                playDrum(i, 2)
+            }
             for(var j=0; j<pitch; j++){
                 let current_pitch=current_melody[j]
                 if (current_pitch!='0'){
                     playPiano(i+1, j+1, current_pitch)
                 }
             }
+            
         }
+    }
+    if(play_controller=="stop"){
+    }
+    if(play_controller=="save"){
+
     }
 }
