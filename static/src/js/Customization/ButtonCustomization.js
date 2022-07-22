5c5d33d92fe2cec85830f9b8b228eacaf3e82935
syeminPark <soccer0874@gmail.com>
2022-07-22 13:44:54 +0900

    fix bug

--- static/src/js/Customization/ButtonCustomization.js
+++ static/src/js/Customization/ButtonCustomization.js
@@ -8,11 +8,14 @@ export class ButtonCustomization {
 
     assignEventHandler(event, callback) {
         for (let child of $("." + this.HTMLClassContainer)[0].children) {
-            $("#" + child.id).on(event, () => {
-                this.parameter = child.id.replace(this.eraseText, '')
-                console.log(this.parameter)
-                callback(this.parameter)
-            })
+            if ($(child)[0].localName == "button") {
+                $("#" + child.id).on(event, () => {
+                    this.parameter = child.id.replace(this.eraseText, '')
+                    console.log(this.parameter)
+                    callback(this.parameter)
+                })
+            }
+            
         }
     }
 
