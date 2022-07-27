2fbd419e713da616bfc7ca7f9c5ef7ddc8122c42
ChungHaLee <ssk04040@naver.com>
2022-07-27 13:26:47 +0900

    edited changePlay.js

--- static/src/js/forUI/changePlay.js
+++ static/src/js/forUI/changePlay.js
@@ -1,12 +1,12 @@
 // play & pause 아이콘을 클릭에 따라 바꾸는 js 파일입니다.
 
-const playGlyph = document.getElementById("playGlyph");
+const playButton = document.getElementById("play");
 let count = 0
 
 playButtonChange();
 
 function playButtonChange(){
-    playGlyph.addEventListener("click", () => {
+    playButton.addEventListener("click", () => {
         count += 1
         if (count % 2 != 0){
             document.querySelector('.glyphicon-play').setAttribute("class", "glyphicon glyphicon-pause");
